13605be3c72ef49d514c88344d5c98950ca21e13
16132250 <jason.curate50@spsmail.cuny.edu>
2023-03-14 20:49:24 -0400

    Testing creating a player class and random dice rolls

--- pig.py
+++ pig.py
@@ -1,4 +1,17 @@
+import random
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+
+    def dice_roll(self):
+        roll = random.randint(1,6)
+        print(f"{self.name} rolled a {roll}")
+        return roll
 
 if __name__ == "__main__":
-    pass
-    
+    player1 = Player("Player 1")
+    rollp1 = player1.dice_roll()
+    player2 = Player("Player 2")
+    rollp2 = player2.dice_roll()
