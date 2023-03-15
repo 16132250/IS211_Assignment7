bcd8e3f86926762e886ba3f90e491fad8ff52dbe
16132250 <jason.curate50@spsmail.cuny.edu>
2023-03-15 17:11:23 -0400

    Testing the input and displaying the option selected

--- pig.py
+++ pig.py
@@ -5,13 +5,24 @@ class Player:
         self.name = name
         self.score = 0
 
-    def dice_roll(self):
-        roll = random.randint(1,6)
-        print(f"{self.name} rolled a {roll}")
-        return roll
+def dice_roll(Player):
+    roll = random.randint(1,6)
+    print(f"{Player.name} rolled a {roll}")
+    return roll
+
+def play_or_stay(Player):
+    choice = input(f"{Player.name} choose 1 to roll, 0 to stay")
+    return choice
+
+
 
 if __name__ == "__main__":
     player1 = Player("Player 1")
-    rollp1 = player1.dice_roll()
+    rollp1 = dice_roll(player1)
+    choicep1 = play_or_stay(player1)
+    print(f"{player1.name} chose: {choicep1}")
+
     player2 = Player("Player 2")
-    rollp2 = player2.dice_roll()
+    rollp2 = dice_roll(player2)
+    choicep2 = play_or_stay(player2)
+    print(f"{player2.name} chose: {choicep2}")
